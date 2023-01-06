8dad029c22b0145ae2b49a75edb9f7efac5e520e
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-01-06 15:23:58 +0100

    modified some files therein

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+// implementation of the toggle section
+const toggle = document.querySelector(".toggle");
+const toggleContainer = document.querySelector(".toggleCT");
+
+(toggle, toggleContainer).addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  toggleContainer.classList.toggle("act");
+});
